b0a91fceaa938a65ef86b35edc994d2d36c80ded
Thiagomatheus12 <thi_ago5@hotmail.com>
2023-01-12 16:56:33 -0300

    :sparkles: feat - criação dos componentes e estilização

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Module
+import { SharedModule } from '../shared/shared.module';
+
 //Module Routing
 import { RoutingModule } from './routing.module';
 
@@ -17,7 +20,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
